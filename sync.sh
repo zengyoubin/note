dc5a0be822c9184e79f88a63adcc17f1c4ad92cb
youbin_zeng <zengyoubin@ahaschool.com>
2020-09-23 16:38:01 +0800

    auto sync

---
+++ sync.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+cd $1
+
+echo `pwd`
+echo 'auto sync start'
+
+git add -A
+nothingStr =`git commit -m 'auto sync' | grep 'nothing to commit'`
+
+if [[ ! -z $nothingStr ]]; then
+	echo $nothingStr
+	exit 0 
+fi
+
+echo '1111'
